494413039477af248a3dcafd5e6ae3232c254f00
StockLin <a6898208@gmail.com>
2022-11-24 18:28:37 +0800

    add login form.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'antd/dist/antd.min.css';
 import { Counter } from './features/counter/Counter';
 import Home from './features/home/Home';
+import Login from './features/login/Login';
 
 const  App : React.FC = () => {
   return (
@@ -11,7 +12,7 @@ const  App : React.FC = () => {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/account/register' element={<div>Register Page</div>} />
-          <Route path='/account/login' element={<div>Login Page</div>} />
+          <Route path='/account/login' element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/features/login/Login.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import styles from './Login.module.css';
+import Footer from '../../commons/components/Footer/Footer';
+import { Button, Divider } from 'antd';
+
+const Login : React.FC = () => {
+  const [formType, setFormType] = useState<'login' | 'register'>('login');
+  const [errorMessage, setErrorMessage] = useState<string>('');
+
+  return (
+    <div>
+      <header>
+        <div className='container'>
+          head
+        </div>
+      </header>
+
+      <div className={styles['main']}>
+        <div className='container'>
+          <div className={styles['main-content']} style={{ backgroundImage: `url('https://cf.shopee.tw/file/407e3f72d67dc2e5fa09ff778a28349e')`}}>
+            <div className={styles['login-content']}>
+              <div className={styles['login-content__title']}>登入</div>
+              <form className={styles['login-content__form']} action="">
+                {
+                  errorMessage &&
+                  <div className="login-content__form--message">error message</div>
+                }
+                <input type="text" placeholder='電話號碼/使用者名稱/Email' />
+                <input type="password" placeholder='密碼' />
+                <Button className={styles['btn-login']} >登入</Button>
+
+                <div className={styles['login-content__form__extras']}>
+                  <a href="#!">忘記密碼</a>
+                  <a href="#!">使用SMS簡訊登入</a>
+                </div>
+              </form>
+            
+              <Divider style={{ color: 'rgba(0, 0, 0, 0.3)', fontSize: 12 }}>或</Divider>
+
+              <div className={styles["oauth-login"]}>
+                <div className={styles["oauth-login__item"]}><i className="fa-brands fa-facebook"></i> Facebook</div>
+                <div className={styles["oauth-login__item"]}><i className="fa-brands fa-google"></i> Google</div>
+                <div className={styles["oauth-login__item"]}><i className="fa-brands fa-apple"></i> Apple</div>
+              </div>
+
+              <div className={styles["new-member"]}>
+                蝦皮新朋友？ <span onClick={() => setFormType('login')}>註冊</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <Footer />
+    </div>
+  )
+}
+
+export default Login;
