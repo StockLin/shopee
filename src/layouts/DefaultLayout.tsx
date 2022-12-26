8caa1364079b97567fef9aa54dc2312cbd2d91c9
StockLin <a6898208@gmail.com>
2022-12-26 23:13:12 +0800

    fix header, footer componet export path error.

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import styles from './DefaultLayout.module.css';
 import Header from '../commons/components/Header/Header';
 import Footer from '../commons/components/Footer/Footer';
@@ -10,15 +11,21 @@ interface IProps {
 }
 
 
-const DefaultLayout : React.FC<IProps> = ({ fixHeader=false, children }) => {
+const DefaultLayout: React.FC<IProps> = ({ fixHeader = false, children }) => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <React.Fragment>
       <Header fixed={fixHeader} />
 
-      { fixHeader && <div className={styles['clearfix']} /> }
+      {fixHeader && <div className={styles['clearfix']} />}
 
       <div className={styles['content']}>
-        { children }
+        {children}
       </div>
 
       <Footer />
